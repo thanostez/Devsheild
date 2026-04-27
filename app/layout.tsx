d65ac2feec496ec61c154c9c461bc83cd20e3d13
Thanos Tez <thanostez@gmail.com>
2026-04-27 19:02:25 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,6 +73,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1270139027361859"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${inter.variable} ${jetBrainsMono.variable} min-h-screen flex flex-col bg-primaryBg font-body text-textPrimary antialiased`}
       >
